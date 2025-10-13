6787d886185190633b3c2dcc30cc3683be85d914
Max Brother <mrmax@mxMBP-2.local>
2025-10-13 15:15:57 -0400

    fixed typo

--- core/context_processor.py
+++ core/context_processor.py
@@ -1,4 +1,11 @@
 from .navigation import NAV_ITEMS
 
-def navigation_context(request):
-    return {"NAV_ITEMS": NAV_ITEMS}
+def nav_links(request):
+    return {
+        "NAV_LINKS": [
+            {"name": "Dashboard", "url": "/"},
+            {"name": "Imports", "url": "/imports/"},
+            {"name": "Recipes", "url": "/recipes/"},
+            {"name": "Inventory", "url": "/inventory/"},
+        ]
+    }
