97ae98f4a84f9998b2676c699f3f328edf8bcc76
Max Brother <mrmax@mxMBP-2.local>
2025-10-16 13:37:58 -0400

    underscore not hyphen

--- core/settings.py
+++ core/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django-browser-reload', #hopefully fixes caching issues
+    'django_browser_reload', #hopefully fixes caching issues
     'mscrInventory', #new app
 ]
 
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
