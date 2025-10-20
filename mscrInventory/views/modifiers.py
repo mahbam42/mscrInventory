5090127a5404734b868b223d38687d677a492b96
Max Brother <mrmax@mxMBP-2.local>
2025-10-20 14:36:57 -0400

    added notes

--- mscrInventory/views/modifiers.py
+++ mscrInventory/views/modifiers.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
-from mscrInventory.models import RecipeModifier
+from mscrInventory.models import RecipeModifier, Ingredient
 
 def edit_modifier_extra_view(request, modifier_id):
     modifier = get_object_or_404(RecipeModifier, pk=modifier_id)
