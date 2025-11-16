30a1630676a38b16bbd0c76ac2930a4cb8a5841c
Max Brother <mrmax@mxMBP-2.local>
2025-11-15 19:34:28 -0500

    fixing up user_management.html and supporting code behind

--- mscrInventory/forms.py
+++ mscrInventory/forms.py
@@ -569,7 +569,7 @@ class PublicUserCreateForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.is_active = True
+        user.is_active = False
         user.is_staff = False
         user.set_password(self.cleaned_data["password1"])
         if commit:
